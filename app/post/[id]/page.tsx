3edb06d4a308475b2fd6c8b84579a81a359fa69f
iambavith <iambavith@gmail.com>
2024-01-02 17:46:22 +0530

    redirect

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,9 +1,18 @@
 import { redirect } from 'next/navigation'
+import { FC } from 'react'
 
-export default async function Home({ params }) {
+interface PostProps {
+    params: {
+        id: string
+    }
+}
+
+const Post:FC<PostProps> = ({ params }) => {
 
     //proscreens://app/post/109303145898302795432_2TJc5G
-    
+
     redirect(`proscreens://app/post/${params?.id}`)
 
 }
+
+export default Post;
